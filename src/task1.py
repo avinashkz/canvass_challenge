11ff64042a80c13bb94d71642d45c1bd5f893dfb
ap26793 <zi.avi7@gmail.com>
2018-07-22 03:06:09 -0700

    Added docs

--- src/task1.py
+++ src/task1.py
@@ -15,6 +15,18 @@ from sklearn.ensemble import RandomForestRegressor
 import _pickle as pickle
 
 def process_data(X, refit = False):
+    
+    """
+    Args:
+        X : Pandas Dataframe
+            A dataframe containing all the features for task 1.
+        refit : Boolean
+            Refit all the models if `True`.
+            
+    Returns:
+        df : Pandas Dataframe
+            A dataframe containing all the features with missing values imputed.
+    """
 
     fixed_values = {}
     
@@ -40,19 +52,24 @@ def process_data(X, refit = False):
 
         print("==== Processing {} ====".format(col_name))
 
-        top_corr = np.argmax(np.abs(columns))
-        second_corr = pd.DataFrame(columns).sort_values(col_name).iloc[-2].name
-        third_corr = pd.DataFrame(columns).sort_values(col_name).iloc[-3].name
+        # Identify the top 3 correlated features for the selected feature
+        sorted_col = pd.DataFrame(columns).sort_values(col_name)
+
+        top_corr = sorted_col.iloc[-1].name
+        second_corr = sorted_col.iloc[-2].name
+        third_corr = sorted_col.iloc[-3].name
 
         features = [top_corr, second_corr, third_corr]
         
+        # Extract target variable and correlated features
         y = all_data.loc[:, col_name]
         x1 = all_data.loc[:, top_corr]
         x2 = all_data.loc[:, second_corr]
         x3 = all_data.loc[:, third_corr]
 
         if refit:        
-
+            
+            # Fit a model to predict the target variable using each of the correlated feature
             print(" \t Fitting")
             for number, item in enumerate([x1, x2, x3]):   
                 X_train = item[np.invert(y.isnull() | item.isnull())].values.reshape(-1, 1)
@@ -61,33 +78,45 @@ def process_data(X, refit = False):
                 #Select Classifier or Regression model based on the number of unique values
                 if(len(y.unique()) <= 7):
                     rf_model = RandomForestClassifier()
+                    
+                    # Boosting categorical variables that has values between 0 and 1.
                     if y_train[0] < 1 : 
                         y_train = y_train*100
+                        
+                    # Classification models cannot handle float.
                     y_train = y_train.astype('int')
-
+                    
+                    # Set most frequent item as back-up if top 3 correlated features are missing.
                     fixed_values[col_name] = mode(y)
 
                 else:
                     rf_model = RandomForestRegressor()
 
+                    # Setting mean of the variable as back-up if top 3 correlated features are missing.                    
                     fixed_values[col_name] = np.mean(y)
 
                 # Fitting Random forest model
                 rf_model.fit(X_train, y_train)
 
-
+                #Save the built model
                 with open('../results/pickles/{}-{}x.pickle'.format(col_name, number), 'wb') as save_model:
                     pickle.dump(rf_model, save_model)
-
+                    
+            # Save the dictionary containing fixed values if all the top 3 correlated features are missing.
             with open('../results/pickles/fixed_values.pickle', 'wb') as save_model:
                 pickle.dump(fixed_values, save_model)
 
         print(" \t Imputing \n")
 
         # Data missing selected feature
+        
+        # Target variable is missing & X1 is present.
         fallback_1 = y.isnull() & np.invert(x1.isnull())
+        # Target variable and X1 are missing. But X2 is present
         fallback_2 = y.isnull() & x1.isnull() & np.invert(x2.isnull())
+        # Target variable, X1 and X2 are missing. But X3 is present
         fallback_3 = y.isnull() & x1.isnull() &  x2.isnull() & np.invert(x3.isnull())
+        # The target variable and all the top 3 correlated features are missing.
         fallback_4 = y.isnull() & x1.isnull() &  x2.isnull() & x3.isnull()
         fallbacks = [fallback_1, fallback_2, fallback_3, fallback_4]
 
